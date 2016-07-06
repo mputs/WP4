b1d73006ccdb998775ae6512cfa9da0a2a1952df
mputs <marco.puts@gmail.com>
2016-07-06 19:08:33 +0000

    added number check of imo number

--- aisframe/src/main/scala/aisframe.scala
+++ aisframe/src/main/scala/aisframe.scala
@@ -5,6 +5,15 @@ import org.apache.spark.SparkConf
 
 object AISframe
 {
+	def checkimo(imo: String): Boolean =
+	{
+		if(imo.length!=7)
+			return false;
+		val x = imo.map(_.toInt-'0'.toInt); 
+		if (((7 to 2 by -1 zip x.dropRight(1)).map{case(a,b)=>a*b}.sum-x(6))%10==0)
+			return true;
+		return false	
+	}
 	def main(args: Array[String])
 	{
 		val hdfsprefix = "hdfs://namenode.ib.sandbox.ichec.ie:8020/" 
@@ -20,7 +29,7 @@ object AISframe
 			.map(_.split(","))
 			.filter(x=> x(0)!="mmsi")
 		
-		val imos = data.map(x => x(1)).distinct().filter(x=>x.length==7);
+		val imos = data.map(x => x(1)).distinct().filter(x=>checkimo(x));
 		imos.saveAsTextFile(outputfile);
 		
 		sc.stop()
