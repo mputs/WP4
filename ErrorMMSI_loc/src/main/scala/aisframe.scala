452ec9fdae5f5f3a8d807af64e49936e9b64cebc
tesdw <tesdw@yahoo.com>
2016-11-07 14:44:07 +0100

    Update aisframe.scala

--- ErrorMMSI_loc/src/main/scala/aisframe.scala
+++ ErrorMMSI_loc/src/main/scala/aisframe.scala
@@ -46,7 +46,7 @@ object AISframe
 		//val locdata = sc.textFile(locdatafile)
 		//		.map(_.split(","))
 		//	.filter(x=> x(0)!="mmsi")
-		val locdata = sc.textFile(locdatafile).map(_.split(",")).filter(x=> x(0)!="mmsi" && x(1).toDouble>=4.7298 && x(1).toDouble<=4.8814  && x(2).toDouble>=52.3878  && x(2).toDouble<=52.4406 )
+		val locdata = sc.textFile(locdatafile).map(_.split(",")).filter(x=> x(0)!="mmsi" && x(1).toDouble>=4.7298 && x(1).toDouble<=4.8814  && x(2).toDouble>=52.3878  && x(2).toDouble<=52.4406 );
 
 
 		val single_mmsi = data.map(x => (x(0), x(2).mkString(",")))
