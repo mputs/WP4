f2ac5574af3f891e077c6711db1ac8583a15a960
tesdw <tesdw@yahoo.com>
2016-11-24 11:22:31 +0100

    Update aisframe.scala

--- Portvisit/src/main/scala/aisframe.scala
+++ Portvisit/src/main/scala/aisframe.scala
@@ -56,13 +56,13 @@ object AISframe
 
 		
 		//val shipframe = data.map(x => (x(0), Array(x(1), x(2)).mkString(",")))
-		val ship_orig = data.map(x=>(x(0), Array(x(1).toString,x(2).toString,x(3).toString,x(4).toString,x(5).toString,x(6).toString)))
-			.groupByKey()
-			.map(x=>(x._1,x._2.toList.sortWith((a,b)=>a(6).toLong<b(6).toLong)))
+		//val ship_orig = data.map(x=>(x(0), Array(x(1).toString,x(2).toString,x(3).toString,x(4).toString,x(5).toString,x(6).toString)))
+		//	.groupByKey()
+		//	.map(x=>(x._1,x._2.toList.sortWith((a,b)=>a(6).toLong<b(6).toLong)))
 
 		//val enters = ship_orig.flatMap(x=>(x._2.map(y=>y(4)).toArray.sliding(2).filter(x(0)!=x(1) && x(1)!="SEA" ).map(x=>x(1)),1)).reduceByKey
-		val enters = ship_orig.flatMap(x=>x._2.map(y=>y(5)).toArray.sliding(2).toArray.filter(x=>x.length>1).map(x=>((x(0),x(1)),1))).filter(x=>x._1._1!="SEA" || x._1._2!="SEA").reduceByKey(_+_)
-		enters.map(a=> Array(a._1._1, a._1._2,a._2).mkString(",")).saveAsTextFile(outputfile);
+		//val enters = ship_orig.flatMap(x=>x._2.map(y=>y(5)).toArray.sliding(2).toArray.filter(x=>x.length>1).map(x=>((x(0),x(1)),1))).filter(x=>x._1._1!="SEA" || x._1._2!="SEA").reduceByKey(_+_)
+		//enters.map(a=> Array(a._1._1, a._1._2,a._2).mkString(",")).saveAsTextFile(outputfile);
 
 
 		sc.stop()
