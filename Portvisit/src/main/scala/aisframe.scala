16aca89ae80fde1faee018ae9dc3f8b6bf62ebcb
mputs <marco.puts@gmail.com>
2016-11-24 12:51:51 +0100

    Update aisframe.scala

--- Portvisit/src/main/scala/aisframe.scala
+++ Portvisit/src/main/scala/aisframe.scala
@@ -54,6 +54,17 @@ object AISframe
 				       val df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 				       it.map(x=>x++Array(findHarbour(x(2).toDouble,x(3).toDouble),df.parse(x(1)).getTime))
 			} 
+               //2015-10-08 22:00:00.001
+                val data = sc.textFile(rawdatafile).map(_.split(","))
+                        .filter(x=>x(0)!="mmsi")
+                        .map(x=> ((x(0), x(8).substring(0,x(8).lastIndexOf(":") ) ),(x(1).toDouble, x(2).toDouble, x(4).toDouble,1.toInt )))
+                        .reduceByKey((a,b)=>(a._1+b._1,a._2+b._2,a._3+b._3,a._4+b._4))
+                        .map(x=>Array(x._1._1,x._1._2,(x._2._1/x._2._4).toString, (x._2._2/x._2._4).toString,(x._2._3/x._2._4).toString))
+                        .mapPartitions{it =>
+                                       val df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                                       it.map(x=>x++Array(findHarbour(x(2).toDouble,x(3).toDouble),df.parse(x(1)).getTime))
+                        }
+
 
 		
 		//val shipframe = data.map(x => (x(0), Array(x(1), x(2)).mkString(",")))
