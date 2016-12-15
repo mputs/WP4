586a1e3e758e939a5a16b84851e09a26b1286b92
mputs <marco.puts@gmail.com>
2016-12-15 16:25:46 +0100

    Update aisframe.scala

--- Portvisit/src/main/scala/aisframe.scala
+++ Portvisit/src/main/scala/aisframe.scala
@@ -70,40 +70,22 @@ object AISframe
 				Median(x._2.toList.map(y=>y._1.toDouble).toList), 
 				Median(x._2.toList.map(y=>y._2.toDouble).toList), 
 				Median(x._2.toList.map(y=>y._3.toDouble).toList) )) )
-			.map(x=> (x._1, (x._2._1, x._2._2, x._2._3, findHarbour(x._2._1, x._2._2))))
-			.map(x=> ((x._1._1),(x._2._2, x._2._1, x._2._2,x._2._3,x._2._4)) )
-			// tuple of ((mmsi), (time, lat, lon, speed, harbour))
+			.map(x=> (x._1, Array(x._2._1, x._2._2, x._2._3, findHarbour(x._2._1, x._2._2))))
+			// tuple of ((mmsi, time), Array(lat, lon, speed, harbour))
 		//orig data7.map(a=>Array(a(1), a(2),a(3),a(4),a(5),a(6)).mkString(",")).saveAsTextFile("hdfs://namenode.ib.sandbox.ichec.ie:8020/user/tessadew/schipje.csv")
 		//orig data: mmsi timestamp lat lon speed harbour time
 
-			
+		val enters = data.map(x=>(x._1._1, (x._1._2, x._2)))
+				.groupByKey()
+				.map(x=>(x._1,x._2.toList
+					 	.sortWith((a,b)=>a._1<b._1)
+					 	.map(_._2(3))
+					 	.sliding(2)
+					 	.toArray
+					 	.filter(x=>x.length>1)))
+				.flatMap(x=>x._2.map(y=>((x._1,y(0),y(1)),1)))
+				.reduceByKey(_+_)	
 
-
-		
-		//val shipframe = data.map(x => (x(0), Array(x(1), x(2)).mkString(",")))
-		val ship_orig = data.map(x=>(x(0).toString, Array(x(1).toString,x(2).toString,x(3).toString,x(4).toString,x(5).toString,x(6).toString)))
-			.groupByKey()
-			.map(x=>(x._1,x._2.toList.sortWith((a,b)=>a(5).toLong<b(5).toLong))).cache
-
-
-		// ship_orig: mmsi, (timestamp, lat, lon, speed, harbour, time)*
-
-		
-
-		//val enters = ship_orig.flatMap(x=>(x._2.map(y=>y(4)).toArray.sliding(2).filter(x(0)!=x(1) && x(1)!="SEA" ).map(x=>x(1)),1)).reduceByKey
-		
-		//val enters = data.groupByKey()
-		//	.flatMap(x=>x._2.toList.sortWith((a,b)=>a._1.toLong<b._1.toLong)
-		//				.map(y=>y._5)
-		//				.sliding(2)
-		//				.toArray
-		//				.filter(x=>x.length>1)
-		//				.map(y=>((x._1,y(0),y(1)),1))   ).reduceByKey(_+_)
-		val enters = data3.map(x=>(x._1._1, (x._1._2, x._2)))
-					.groupByKey().map(x=>(x._1,x._2.toList.sortWith((a,b)=>a._1<b._1)
-					.map(_._2(2)).sliding(2).toArray
-					.filter(x=>x.length>1))).flatMap(x=>x._2.map(y=>((x._1,y(0),y(1)),1)))
-					.reduceByKey(_+_)
 		
 		//val enters = ship_orig.flatMap(x=>x._2.map(y=>y(4))
 		//			       		.toArray.sliding(2).toArray
@@ -111,19 +93,19 @@ object AISframe
 		//			       		.map(x=>((x(0),x(1)),1)) )
 		//			       	.filter(x=>x._1._1!="SEA" || x._1._2!="SEA")
 		//			       .reduceByKey(_+_)
-		//enters.map(a=> Array(a._1._1, a._1._2,a._2).mkString(",")).saveAsTextFile(outputfile);
+		enters.map(a=> Array(a._1._1, a._1._2,a._2).mkString(",")).saveAsTextFile(outputfile);
 
 		
 
 
-		val getshipsinharbour = ship_orig.flatMap(x=>x._2.map(y=>y(4))
-					       		.toArray.sliding(2).toArray
-					       		.filter(z=>z.length>1)
-					       		.map(z=>((z(0),z(1),x._1),1)) )
-							//.filter(x=> x._1._3 =="304783000")
-					       //.filter(x=>x._1._1!="SEA" || x._1._2!="SEA")
-					       .reduceByKey(_+_)
-		getshipsinharbour.map(a=> Array(a._1._1, a._1._2,a._1._3, a._2).mkString(",")).saveAsTextFile(outputfile);
+		//val getshipsinharbour = ship_orig.flatMap(x=>x._2.map(y=>y(4))
+		//			       		.toArray.sliding(2).toArray
+		//			       		.filter(z=>z.length>1)
+		//			       		.map(z=>((z(0),z(1),x._1),1)) )
+		//					//.filter(x=> x._1._3 =="304783000")
+		//			       //.filter(x=>x._1._1!="SEA" || x._1._2!="SEA")
+		//			       .reduceByKey(_+_)
+		//getshipsinharbour.map(a=> Array(a._1._1, a._1._2,a._1._3, a._2).mkString(",")).saveAsTextFile(outputfile);
 
 		sc.stop()
 	}
