009f4d463615421cfd7f4040f91d21549bd28c1b
tesdw <tesdw@yahoo.com>
2017-01-11 14:14:13 +0100

    Update aisframe.scala

--- Portvisit/src/main/scala/aisframe.scala
+++ Portvisit/src/main/scala/aisframe.scala
@@ -21,7 +21,7 @@ object AISframe
 		val rawdatafile = hdfsprefix + args(1)
 		//val outputfile = hdfsprefix + args(2)
 		val outputfile_arr = hdfsprefix + args(2)
-		val outputfile_dep = hdfsprefix + args(2)
+		val outputfile_dep = hdfsprefix + args(3)
 		
 		//val seaships = "hdfs://namenode.ib.sandbox.ichec.ie:8020/user/tessadew/defframe6all.csv"
 		//val rawdatafile = "hdfs://namenode.ib.sandbox.ichec.ie:8020/datasets/AIS/Locations/2015120100*.csv.gz"
