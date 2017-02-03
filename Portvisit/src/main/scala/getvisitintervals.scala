f36cf3dab70ab3b1ea9064eb37d4ab20258170cb
= <=>
2017-02-03 19:36:39 +0100

    resolved errors

--- Portvisit/src/main/scala/getvisitintervals.scala
+++ Portvisit/src/main/scala/getvisitintervals.scala
@@ -4,7 +4,7 @@ object visitinterval
 		def getvisitinterval(arrivals: List[List[String]], departures: List[List[String]]):List[List[Long]] = 
 		{
 			var ready = false;
-			var portvisits = List[Array[Long]]()
+			var portvisits = List[List[Long]]()
 			//set initial arrival
 			var arrivHarbour = arrivals(0)(0);
 			var arrivTime = arrivals(0)(1).toLong;
@@ -15,7 +15,7 @@ object visitinterval
 				if (dep> -1)
 				{
 					depTime = departures(dep)(1).toLong
-					portvisits = portvisits ++ List(Array(arrivTime, depTime))
+					portvisits = portvisits ++ List(List(arrivTime, depTime))
 
 					var arr = arrivals.indexWhere(x=>x(1).toLong>depTime)
 					if(arr== -1) 
