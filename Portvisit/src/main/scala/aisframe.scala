fb39827118e913861afe12f1f2c871b6e764a7bb
tesdw <tesdw@yahoo.com>
2017-05-03 15:19:19 +0200

    Update aisframe.scala

--- Portvisit/src/main/scala/aisframe.scala
+++ Portvisit/src/main/scala/aisframe.scala
@@ -1,4 +1,3 @@
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -7,7 +6,6 @@ import java.text._;
 import java.util.Date;
 import visitinterval._;
 import intervals_connect._
-import scala.annotation.tailrec
 //import org.apache.spark.sql.functions._ 
 
 object AISframe
@@ -91,35 +89,6 @@ object AISframe
    			 l.map(x=>(x(0) until x(1)+1).toList).flatten
 			}
 		
-		def connectIntervals(l:List[List[Long]]): List[List[Long]] = 
-			{
-		// tailrecursive helper function
-			@tailrec def rec2_connectIntervals(out: List[List[Long]], begin: Long, end: Long, l:List[List[Long]]): List[List[Long]] = 
-			{
-				// base case: when list l is empty: return the result and terminate
-				if(l.length == 0) return List(begin,end)::out  
-				if(l.head(0) - end > 1) 
-				//inductive step1: when the time between the time intervals is large, just add the last interval
-				rec2_connectIntervals(List(begin,end)::out, l.head(0), l.head(1),l.tail)
-			else
-				//nductive step2: when the time between the time intervals is small, extend the interval
-				rec2_connectIntervals(out, begin, l.head(1),l.tail)
-			}
-		if (l.length > 0)
-			//call helper function
-			return rec2_connectIntervals(List(): List[List[Long]],l.head(0), l.head(1), l.tail)
-		else 
-			return List(): List[List[Long]]
-			}
-
-		def haversineDistance(pointA: (Double, Double), pointB: (Double, Double)): Double = {
-  			val deltaLat = math.toRadians(pointB._1 - pointA._1)
-  			val deltaLong = math.toRadians(pointB._2 - pointA._2)
-  			val a = math.pow(math.sin(deltaLat / 2), 2) + math.cos(math.toRadians(pointA._1)) * math.cos(math.toRadians(pointB._1)) * math.pow(math.sin(deltaLong / 2), 2)
-  			val greatCircleDistance = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-  			6371 * greatCircleDistance
-			}
-		
                //2015-10-08 22:00:00.001
 		val rawdata = sc.textFile(rawdatafile)
 				.map(_.split(","))
@@ -153,39 +122,20 @@ object AISframe
 		val ar = arrs.map(x=>((x(0),x(1)),List(x(1),x(2)))).groupByKey().map(x=>(x._1,x._2.toList))
 		val de = deps.map(x=>((x(0),x(1)),List(x(1),x(2)))).groupByKey().map(x=>(x._1,x._2.toList))
 		val grouped = ar.join(de)
+		val intervals = grouped
+			.map(x=>(x._1,getvisitinterval(x._2._1.map(_.map(_.toString)), x._2._2.map(_.map(_.toString)))))
+			.filter(x=> x._2.length!=0)
+			.map(x=>(x._1, connectIntervals(x._2)))
+		val expandedintervals = intervals
+			//.flatMap(x=>expandIntervals(x._2).map(y=>((x._1._1, y.toString),  (x._1._2, x._2(0)(0),x._2(0)(1))))).groupByKey().map(x=>(x._1.toString,x._2.toList))
+			.flatMap(x=>x._2.flatMap(y=>(y(0) until y(1)+1).toList.map(z=>((x._1._1.toString, z.toString), (x._1._2, y.mkString(","))))))
 		
 		
-		
-// ((mmsi, port), List(List(begin, end),..)
-val intervals = grouped.map(x=>(x._1,getvisitinterval(x._2._1.map(_.map(_.toString)), x._2._2.map(_.map(_.toString))))).filter(x=> x._2.length!=0).map(x=>(x._1, connectIntervals(x._2)))
-
-val expendedintervals = intervals
-		.flatMap(x=>x._2.flatMap(y=>(y(0) until y(1)+1).toList.map(z=>((x._1._1.toString, z.toString), (x._1._2, y.mkString(","))))))
-val expandedintervals = intervals
-		.flatMap(x=>expandIntervals(x._2).map(y=>((x._1._1, y.toString),  (x._1._2, x._2(0)(0),x._2(0)(1)))))
-		.groupByKey()
-		.map(x=>(x._1.toString,x._2.toList))
-
-
-val arrdep2 = data.map(x=>(x._1,(x._2(0), x._2(1),x._2(2), x._2(3))))
-val exp_int_compl = expendedintervals.join(arrdep2)
-val int_speed = exp_int_compl.map(x=> ((x._1._1,x._2._1._2),(x._2._2._1),(x._2._2._2),(x._2._2._3),(x._1._2)))//.saveAsTextFile(outputfile)
-val distance2= int_speed.map(x=>(x._1,(x._5, x._2, x._3)))
-		.groupByKey().map(x=>(List(x._1),x._2.toList.sortWith((a,b)=>a._1<b._1).map(l=>List(l._1,l._2,l._3))))
-val distance3 = distance2.map(q=>(q._1,q._2.sliding(2).toList.map(x=>haversineDistance((x(0)(2).asInstanceOf[Double],x(0)(1).asInstanceOf[Double]), 
-                                                    (x(1)(2).asInstanceOf[Double],x(1)(1).asInstanceOf[Double]))).sum))
-						    
-def tuple3ToList[T](t: (T,T,T)): List[T] = List(t._1, t._2,t._3)
-val arrdep3 = data.map(x=>(x._1.toString,(x._2(0), x._2(1),x._2(2), x._2(3))))
-val exp_int_compl2 = expandedintervals.join(arrdep3).saveAsTextFile(outputfile)
-//val int_speed2 = exp_int_compl2.map(x=> (x._1.split(",")(0).substring(1)::tuple3ToList(x._2._1(0)),x._2._2)).groupByKey().map(x=>(x._1,x._2.toList.sortWith((a,b)=>a._1<b._1).map(l=>List(l._1,l._2,l._3,l._4, l._5))))
-//
-//val dist = int_speed2.map(q=>(q._1,q._2.sliding(2).toList.map(x=>haversineDistance((x(0)(2).asInstanceOf[Double],x(0)(1).asInstanceOf[Double]), 
-//                                                   (x(1)(2).asInstanceOf[Double],x(1)(1).asInstanceOf[Double]))).sum,q._2.sliding(1).toList.map(x=>(x(0)(1).asInstanceOf[Double],x(0)(3).asInstanceOf[Double]))))//.saveAsTextFile(outputfile)
-//val stops = int_speed2.filter(x=>(x._2.sliding(1).toList.map(x=>(x(0)(3))).count(_.asInstanceOf[Double]>0.9))>1).saveAsTextFile(outputfile)//.map(x=> x._1 + ""+x._2.mkString(","))
+		//val arrdep2 = data.map(x=>(x._1.toString,List(x._2)))
+		val arrdep2 = data.map(x=>(x._1,(x._2(0), x._2(1),x._2(2), x._2(4))))
+		val exp_int_compl = expandedintervals.join(arrdep2).saveAsTextFile(outputfile)
 			
 
 		sc.stop()
 	}
-	
 }
