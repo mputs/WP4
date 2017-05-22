f0e823118c886862aa60afe5a850a6c377656465
tesdw <tesdw@yahoo.com>
2017-05-22 13:16:44 +0200

    Update aisframe.scala

--- Portvisit/src/main/scala/aisframe.scala
+++ Portvisit/src/main/scala/aisframe.scala
@@ -143,7 +143,7 @@ object AISframe
 			
 		val arrdep2 = data.map(x=>(x._1,(x._2(0), x._2(1),x._2(2), x._2(3))))
 		val exp_int_compl = expandedintervals.join(arrdep2)
-		val int_speed = exp_int_compl.map(x=> ((x._1._1,x._2._1._2),(x._2._2._1),(x._2._2._2),(x._2._2._3),(x._2._2._4))).saveAsTextFile(outputfile)
+		val int_speed = exp_int_compl.map(x=> ((x._1._1,x._2._1._2),(x._2._2._1),(x._2._2._2),(x._2._2._3),(x._2._2._4),x._1._2)).saveAsTextFile(outputfile)
 
 		sc.stop()
 	}
