80b1c597cf28b0a22ea5ecb1ecf2d10e5404716e
Ioannis Tsalamanis <itsalamanis@gmail.com>
2017-06-29 16:38:55 +0200

    Update aistest.scala

--- aisRoute/src/main/scala/aistest.scala
+++ aisRoute/src/main/scala/aistest.scala
@@ -17,8 +17,9 @@ object AIStest
 		val data = sc.textFile(tfiles);
 		val data1 = data.map(_.split(","));
 		val data2 = data1.filter(x => x(0)=="244670084");
-		//data2.map(_.mkString(",")).saveAsTextFile("/ichec/home/users/mputs/WP4/output")
-		println ("Lines in dataset: %d".format(data2.count()));
+		data2.map(_.mkString(",")).saveAsTextFile("file://hdfs://namenode.ib.sandbox.ichec.ie:8020/")
+		//data2.map(_.mkString(",")).saveAsTextFile("file:///ichec/home/users/mputs/WP4/output")
+		//println ("Lines in dataset: %d".format(data2.count()));
 		sc.stop()
 	}
 }
