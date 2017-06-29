9e72f1b648a4acb69490c6abc86f4c4c273f2c92
Ioannis Tsalamanis <itsalamanis@gmail.com>
2017-06-29 16:55:24 +0200

    Update aistest.scala

--- aisRoute/src/main/scala/aistest.scala
+++ aisRoute/src/main/scala/aistest.scala
@@ -17,7 +17,7 @@ object AIStest
 		val data = sc.textFile(tfiles);
 		val data1 = data.map(_.split(","));
 		val data2 = data1.filter(x => x(0)=="244670084");
-		data2.map(_.mkString(",")).saveAsTextFile("file:///~/WP4/output")
+		data2.map(_.mkString(",")).coalesce(1,true).saveAsTextFile("file:///~/WP4/output")
 		//data2.map(_.mkString(",")).coalesce(1,true).saveAsTextFile("file://~/WP4/output")
 		//println ("Lines in dataset: %d".format(data2.count()));
 		sc.stop()
