b7723b15a7afb87254013df9401e1c046bb8fb64
mputs <marco.puts@gmail.com>
2017-07-18 11:56:12 +0200

    added lat/lons of cells

--- locations/src/main/scala/LAEA.scala
+++ locations/src/main/scala/LAEA.scala
@@ -124,6 +124,17 @@ object LAEA
 			else
 				return (-1,-1)
 		}
+
+		def getlatlonmid(lat: Double, lon: Double): Tuple2[Double, Double] = 
+		{
+			val (xi, yi) = getlatlonidx(lat,lon);
+			if(xi==-1 || yi==-1) 
+				return (xi, yi, -999.0,-999.0)
+			else
+				val (clat, clon) = laea.Inverse(xmin+dx*(xi+0.5), ymin+dy*(yi+0.5));
+				return (xi, yi, clat, clon);
+			
+		}
 	}
 }
 
