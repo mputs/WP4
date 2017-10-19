6ac69ad8341df18f52f1d9fc482ddd0750cdd756
Ioannis Tsalamanis <itsalamanis@gmail.com>
2017-10-19 13:35:42 +0100

    Update aistest.scala

--- aisRoute/src/main/scala/aistest.scala
+++ aisRoute/src/main/scala/aistest.scala
@@ -17,7 +17,7 @@ object AIStest
 		val data = sc.textFile(tfiles);
 		val data1 = data.map(_.split(","));
 		//val data2 = data1.filter(x => x(0)=="235037917");
-		data2.map(_.mkString(",")).saveAsTextFile("hdfs://namenode.ib.sandbox.ichec.ie:8020/user/itsala/output")
+		data1.map(_.mkString(",")).saveAsTextFile("hdfs://namenode.ib.sandbox.ichec.ie:8020/user/itsala/output")
 		//data2.map(_.mkString(",")).saveAsTextFile("file:////ichec/home/users/mputs/WP4/output")
 		//println ("Lines in dataset: %d".format(data2.count()));
 		sc.stop()
